ca665d410f949d14c69e7acfd1c05d673955a0ef
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2026-05-19 15:16:52 +0200

    🧹 Formatting

--- Package.swift
+++ Package.swift
@@ -20,25 +20,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "KituraCache",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "KituraCache",
-            targets: ["KituraCache"]),
-        ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "KituraCache",
-            dependencies: []),
-        .testTarget(
-            name: "KituraCacheTests",
-            dependencies: ["KituraCache"]),
-        ]
+  name: "KituraCache",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "KituraCache",
+      targets: ["KituraCache"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "KituraCache",
+      dependencies: []),
+    .testTarget(
+      name: "KituraCacheTests",
+      dependencies: ["KituraCache"]),
+  ]
 )
